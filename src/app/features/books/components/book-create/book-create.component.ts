72452204104f5fe0939280ab60969d7dd4ae9f03
Hudjoubert <hudson_joubert@yahoo.com.br>
2022-05-20 03:32:22 -0300

    create book

--- src/app/features/books/components/book-create/book-create.component.ts
+++ src/app/features/books/components/book-create/book-create.component.ts
@@ -1,4 +1,8 @@
+import { BookIn } from './../../../../core/models/book-in';
+import { BooksService } from './../../services/books.service';
+import { SharedService } from './../../../../shared/services/shared.service';
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-book-create',
@@ -8,9 +12,27 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 })
 export class BookCreateComponent implements OnInit {
 
-  constructor() { }
+  book: BookIn = {
+    name: '',
+    year: 0,
+    authors: [''],
+    summary: ''
+  }
+
+  constructor(
+    public dialogRef: MatDialogRef<BookCreateComponent>,
+    private booksService: BooksService,
+    private sharedService: SharedService) { }
 
   ngOnInit(): void {
+  }
+
+  addNewBook() {
+    this.booksService.addNewBook(this.book).subscribe(() => {
+      this.sharedService.showMessage('New book created');
+      this.dialogRef.close();
+
+    });
   }
 
 }
